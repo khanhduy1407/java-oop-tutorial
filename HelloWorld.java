23fb3e0d0f2c033c53187660723c29b634482a59
NKDuy <kn145660@gmail.com>
2023-01-23 13:57:18 +0700

    begin oop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Person[] a = new Person[2];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = new Person();
+            a[i].name = "NKDuy no." + i;
+            a[i].age = 21 + i;
+            a[i].height = 0.7f + i;
+        }
+
+        for (Person person: a) {
+            System.out.println(person.name);
+            System.out.println(person.height);
+            System.out.println(person.age);
+        }
     }
 }
