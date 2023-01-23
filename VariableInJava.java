df4a6f0eae5d29fa86555668d4a607866c526d88
NKDuy <kn145660@gmail.com>
2023-01-23 09:14:18 +0700

    variable in java

---
+++ VariableInJava.java
@@ -0,0 +1,26 @@
+public class VariableInJava {
+    public static void main(String[] args) {
+        String name;
+        float height;
+        int age;
+
+        name = "NKDuy";
+        height = 1.7f;
+        age = 21;
+
+        // c1
+        System.out.println(name);
+        System.out.println(height);
+        System.out.println(age);
+        System.out.println("=======================================");
+
+        // c2
+        System.out.print("Ten: ");
+        System.out.println(name);
+        System.out.print("Chieu cao: ");
+        System.out.println(height);
+        System.out.print("Tuoi: ");
+        System.out.println(age);
+        System.out.println("=======================================");
+    }
+}
