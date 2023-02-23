a5717034850e6bdeaef8af842da141a2664e23fb
NKDuy <kn145660@gmail.com>
2023-02-23 08:09:24 +0700

    fix constructor

--- src/hs/Demo1.java
+++ src/hs/Demo1.java
@@ -3,11 +3,18 @@ package hs;
 public class Demo1 {
 
     public static void main(String[] args) {
-        DSHS dshs = new DSHS();
+//        DSHS dshs = new DSHS();
+//
+//        dshs.inputN();
+//        dshs.init();
+//        dshs.inputDSHS();
+//        dshs.outputDSHS();
 
-        dshs.inputN();
-        dshs.init();
-        dshs.inputDSHS();
-        dshs.outputDSHS();
+        HocSinh h1 = new HocSinh();
+        h1.output();
+        HocSinh h2 = new HocSinh("123", "Nguyễn Khánh Duy", 8);
+        h2.output();
+        HocSinh h3 = new HocSinh(h2);
+        h3.output();
     }
 }
